0f3afd3b1cfe0cec1f220c935012712331bef613
Rachel <rachelmramirez@gmail.com>
2015-06-08 19:00:46 -0400

    fixing remove_entry assignment

--- models/address_book.rb
+++ models/address_book.rb
@@ -18,14 +18,7 @@ class AddressBook
     @entries.insert(index, Entry.new(name, phone, email))
   end
 
-    def remove_entry(name, phone, email)
-      index = 0
-      @entries.each do |entry|
-        if name < entry.name
-          break
-        end
-        index += 1
-      end
-    @entries.insert(index, Entry.new(name, phone, email))
+  def remove_entry(entry)
+    @entries.delete(entry)  
   end
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -38,19 +38,12 @@ RSpec.describe AddressBook do
   context ".remove_entry" do
     it "removes only one entry to address book" do
       book = AddressBook.new
-      book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
-
-      expect(book.entries.size).to eq 1
-    end
-
-    it "removes the correct information to entries" do
-      book = AddressBook.new
       book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
-      new_entry = book.entries[0]
-
-      expect(new_entry.name).to eq 'Ada Lovelace'
-      expect(new_entry.phone_number).to eq '010.012.1815'
-      expect(new_entry.email).to eq 'augusta.king@lovelace.com'
+      book.add_entry('Rachel Ramirez', '650.834.4630', 'rachelmramirez@gmail.com')
+      expect(book.entries.size).to eq 2
+      ada = book.entries[0]
+      book.remove_entry(ada)
+      expect(book.entries.size).to eq 1
     end
   end
 end
