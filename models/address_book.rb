4fe0cec7e79724ceee27520e1f91053b6ab3e2e6
Rachel <rachelmramirez@gmail.com>
2015-06-10 18:22:46 -0400

    csv import

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
 require_relative "entry.rb"
+require "csv"
 
 class AddressBook
   attr_accessor :entries
@@ -20,5 +21,17 @@ class AddressBook
 
   def remove_entry(entry)
     @entries.delete(entry)  
+  end
+
+  def import_from_csv(file_name)
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true)
+
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
+
+    return csv.count
   end
 end
